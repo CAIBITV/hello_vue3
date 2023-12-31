6d8dbffb67f1315770de7f74c918e3cf61e3b6b3
caibitv <caibitv@192.168.10.102>
2023-12-31 16:41:15 +0800

    006_setup语法糖

---
+++ src/components/Person.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{name}}</h2>
+    <h2>年龄：{{age}}</h2>
+    <h2>地址：{{ address }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  defineOptions({
+    // 宏定义
+    name: 'Person123'
+  })
+  // 数据，原来写在data中（注意：此时的name、age、tel数据都不是响应式数据）
+  let name = '张三'
+  let age = 18
+  let tel = '13888888888'
+  let address = '北京市'
+
+  // 方法
+  function changeName() {
+    name = 'zhang-san' // 注意：此时这么修改name页面是不变化的
+    console.log(name)  // name确实改了，但name不是响应式的
+  }
+
+  function changeAge() {
+    age += 1
+  }
+
+  function showTel() {
+    alert(tel)
+  }
+</script>
+
+<style scoped>
+  .person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+  button {
+    margin: 0 5px;
+  }
+</style>
---
+++ 备份/002_一个简单的效果/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="app">
+        <h1>你好啊！</h1>
+        <Person/>
+    </div>
+</template>
+
+<script lang="ts">
+  // JS或TS
+  import Person from './components/Person.vue'
+  export default {
+    components: { Person },
+    name: 'App' // 组件名
+  }
+</script>
+
+<style>
+  /* 样式 */
+  .app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
