66d0f6ba8eab1b4f02df213ae5d9640029b444a4
CAIBITV <yyt14588@gmail.com>
2024-01-18 13:17:36 +0800

    feat:  026_路由_to的两种写法

---
+++ 备份/026_路由_to的两种写法/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img src="http://www.atguigu.com/images/index_new/logo.png" alt="">
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineOptions({
+    name: 'Home'
+  })
+  import { onMounted, onUnmounted } from 'vue'
+  onMounted(() => {
+    console.log('Home mounted!')
+  })
+
+  onUnmounted(() => {
+    console.log('Home unmounted!')
+  })
+</script>
+  
+<style scoped>
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+</style>
